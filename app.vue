8a1a58560192c2548cefa70de7214168b69ea050
Xicri <xicri@proton.me>
2022-10-22 19:06:53 +0900

    chore: setup canonical & og:url

--- app.vue
+++ app.vue
@@ -5,3 +5,18 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+const route = useRoute();
+
+const canonical = `https://example.com${route.path.replace(/\/$/, "")}/`; // with trailing slash
+
+useHead({
+  meta: [
+    { name: "og:url", content: canonical },
+  ],
+  link: [
+    { rel: "canonical", href: canonical },
+  ],
+});
+</script>
