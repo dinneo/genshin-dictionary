15470517be5aa6404f789faa8919d721c143ba07
Xicri <xicri@proton.me>
2022-10-23 14:48:20 +0900

    tmp

--- middleware/trailing-slash.global.js
+++
@@ -1,5 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  if (!route.path.endsWith("/")) {
-    return navigateTo(route.path + "/", { redirectCode: 301 });
-  }
-});
---
+++ server/middleware/trailing-slash.js
@@ -0,0 +1,6 @@
+export default defineEventHandler((event) => {
+console.log(event)
+  // if (!route.path.endsWith("/")) {
+  //   return navigateTo(route.path + "/", { redirectCode: 301 });
+  // }
+});
